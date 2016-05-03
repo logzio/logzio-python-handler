3bd9f7844895cf33040e606dbfd556ffda8b21d6
Roi Rav-Hon <roi@logz.io>
2016-05-03 12:14:37 +0300

    License update before release

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="logzio-python-handler",
-    version='1.0.1',
+    version='1.0.2',
     description="Logging handler to send logs to your Logz.io account with bulk SSL",
     keywords="logging handler logz.io bulk https",
     author="roiravhon",
     author_email="roi@logz.io",
     url="https://github.com/logzio/logzio-python-handler/",
-    license="MIT",
+    license="Apache License 2",
     packages=find_packages(),
     install_requires=[
         "requests"
@@ -19,7 +19,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7'
     ]
 )
